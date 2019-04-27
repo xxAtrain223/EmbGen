336cc8e10c4d757e28374ce50d3e33602378b802
Anthony Stewart <anthony.l.stewart@outlook.com>
2019-04-27 18:12:34 -0500

    Implemented Include

--- parser/src/Include.cpp
+++ parser/src/Include.cpp
@@ -1,4 +1,6 @@
 #include "EmbGen/Include.hpp"
+#include <tinyxml2.h>
+#include "EmbGen/Exceptions.hpp"
 
 namespace emb
 {
@@ -9,16 +11,34 @@ namespace emb
             Include::Include(const tinyxml2::XMLElement* xml) :
                 XmlElement(xml)
             {
+                try
+                {
+                    m_standard = getAttribute("standard")->BoolValue();
+                }
+                catch (AttributeException)
+                {
+                    m_standard = false;
+                }
+
+                if (!isAttributesEmpty())
+                {
+                    throw AttributeException("Extra attributes for Include on line " + std::to_string(getLineNum()));
+                }
+
+                if (!isElementsEmpty())
+                {
+                    throw ElementException("Extra elements for Include on line " + std::to_string(getLineNum()));
+                }
             }
 
             std::string Include::getValue() const
             {
-                return "";
+                return getText();
             }
 
             bool Include::isStandard() const
             {
-                return false;
+                return m_standard;
             }
         }
     }
