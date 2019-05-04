b6186dc0a95a91aa01171dfab43e32cf35d72183
Anthony Stewart <anthony.l.stewart@outlook.com>
2019-05-04 18:20:48 -0500

    Implemented Setup

--- parser/src/Setup.cpp
+++ parser/src/Setup.cpp
@@ -1,4 +1,6 @@
 #include "EmbGen/Setup.hpp"
+#include <tinyxml2.h>
+#include "EmbGen/Exceptions.hpp"
 
 namespace emb
 {
@@ -9,11 +11,30 @@ namespace emb
             Setup::Setup(const tinyxml2::XMLElement* xml) :
                 XmlElement(xml)
             {
+                for (auto code : getElements("code"))
+                {
+                    m_code.emplace_back(code);
+                }
+
+                if (m_code.empty())
+                {
+                    throw ElementException("No code for Setup on line " + std::to_string(getLineNum()));
+                }
+
+                if (!isAttributesEmpty())
+                {
+                    throw AttributeException("Extra attributes for Setup on line " + std::to_string(getLineNum()));
+                }
+
+                if (!isElementsEmpty())
+                {
+                    throw ElementException("Extra elements for Setup on line " + std::to_string(getLineNum()));
+                }
             }
 
             std::vector<Code> Setup::getCode() const
             {
-                return std::vector<Code>();
+                return m_code;
             }
         }
     }
