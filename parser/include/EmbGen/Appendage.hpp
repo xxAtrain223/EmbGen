9c0ffc0bda1eb95505c055f62a0816362511d60e
Anthony Stewart <anthony.l.stewart@outlook.com>
2019-05-12 18:16:06 -0500

    Added tests for Appendage [ci skip]

--- parser/include/EmbGen/Appendage.hpp
+++ parser/include/EmbGen/Appendage.hpp
@@ -2,6 +2,15 @@
 #define EMBGEN_PARSER_APPENDAGE_HPP
 
 #include "EmbGen/XmlElement.hpp"
+#include "EmbGen/Command.hpp"
+#include "EmbGen/Include.hpp"
+#include "EmbGen/Loop.hpp"
+#include "EmbGen/Setup.hpp"
+#include "EmbGen/Variable.hpp"
+
+#include <vector>
+#include <map>
+#include <memory>
 
 namespace emb
 {
@@ -15,6 +24,22 @@ namespace emb
 
             public:
                 Appendage(const tinyxml2::XMLElement* xml);
+
+                std::string getName() const;
+
+                std::string getVersion() const;
+
+                std::string getLibDeps() const;
+
+                std::vector<Include> getIncludes() const;
+
+                std::vector<Variable> getVariables() const;
+
+                std::shared_ptr<Setup> getSetup() const;
+
+                std::shared_ptr<Loop> getLoop() const;
+
+                std::map<std::string, Command> getCommands() const;
             };
         }
     }
--- parser/src/Appendage.cpp
+++ parser/src/Appendage.cpp
@@ -10,6 +10,46 @@ namespace emb
                 XmlElement(xml)
             {
             }
+            
+            std::string Appendage::getName() const
+            {
+                return "";
+            }
+            
+            std::string Appendage::getVersion() const
+            {
+                return "";
+            }
+            
+            std::string Appendage::getLibDeps() const
+            {
+                return "";
+            }
+
+            std::vector<Include> Appendage::getIncludes() const
+            {
+                return std::vector<Include>{};
+            }
+
+            std::vector<Variable> Appendage::getVariables() const
+            {
+                return std::vector<Variable>{};
+            }
+
+            std::shared_ptr<Setup> Appendage::getSetup() const
+            {
+                return nullptr;
+            }
+
+            std::shared_ptr<Loop> Appendage::getLoop() const
+            {
+                return nullptr;
+            }
+
+            std::map<std::string, Command> Appendage::getCommands() const
+            {
+                return std::map<std::string, Command>{};
+            }
         }
     }
 }
