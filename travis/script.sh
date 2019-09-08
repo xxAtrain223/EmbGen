9fb0f8af4505e623f5c95f74a6a80bf9f889e0c3
Anthony Stewart <anthony.l.stewart@outlook.com>
2019-09-08 17:04:16 -0500

    Split EmbGenParser into a seperate repo

--- travis/script.sh
+++ travis/script.sh
@@ -4,20 +4,8 @@ set -e
 pushd build
 make -j2
 
-pushd parser
-./EmbGenParserTest
-popd
-
-pushd gen
-./_EmbGenTest
-popd
+./EmbGenTest
 
 popd
 
-pushd parser
-cppcheck src/*.cpp include/Parser/EmbGen/*.hpp
-popd
-
-pushd gen
 cppcheck src/*.cpp include/EmbGen/*.hpp
-popd
